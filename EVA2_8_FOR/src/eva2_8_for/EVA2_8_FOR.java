7384dcccc37644b6511e49cf9b631840b1ef1247
victor alejandro saenz herrera <victoralejandrosaenz07@gmail.com>
2025-10-03 21:04:29 -0600

    Add files via upload

---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author Alejandro Saenz
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 1; i <= 10; i++){
+            System.out.println("Hola mundo ya amanecio");
+        }
+        for(int i = 10; i >= 1; i--){
+            System.out.println("HOLA MUNDO");
+        }
+    }
+    
+}
