73425649a3833448304ac98d236fda4c2eea377d
victor alejandro saenz herrera <victoralejandrosaenz07@gmail.com>
2025-10-03 20:00:05 -0600

    Add files via upload

---
+++ EVA2_7_ARREGLO(25550472/src/eva2_7_arreglo/pkg25550472/EVA2_7_ARREGLO25550472.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_7_arreglo.pkg25550472;
+
+/**
+ *
+ * @author Alejandro Saenz
+ */
+public class EVA2_7_ARREGLO25550472 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cade = "Hola mundo!!";
+        System.out.println(cade.charAt(0));
+        char caracter = cade.charAt(11);
+        System.out.println(caracter);
+        System.out.println(cade.length());
+        int valor = 10;
+    }
+    
+}
